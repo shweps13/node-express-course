dfcb3adc4818f22670b4aa29b321f9334beefe6e
Joe Siburov <g.siburov@gmail.com>
2025-12-16 22:20:28 -0800

    feat(controllers): skeleton added

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,17 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const port = 3000
+
+const tasks = require('./routes/tasks')
+
+app.use(express.json())
+
+app.use('/api/v1/tasks', tasks)
+
+
+
+app.get('/hello', (req, res) => {
+    res.send('Test')
+})
+
+app.listen(port, console.log(`started on ${port}`))
